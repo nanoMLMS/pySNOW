0f3a7ebeeede567993dc36d51bb1b4ae16b76b9f
Giacomo Becatti <giacomo.becatti@unimi.it>
2024-12-09 12:01:04 +0100

    Added tests for cn and gcn on 111 fcc srfc

--- test/test_cn.py
+++ test/test_cn.py
@@ -1,4 +1,4 @@
-from snow.lodispp.utils import coordination_number
+from snow.lodispp.utils import coordination_number, agcn_calculator
 import numpy as np
 from snow.lodispp.pp_io import read_xyz
 def test_cn_isolated():
@@ -26,4 +26,11 @@ def test_fcc():
 def test_bcc():
     el, coords = read_xyz("bcccell.xyz")
     assert coordination_number(1, coords=coords , cut_off = 1.83)[4] == 8
-    
+
+def test_cn_111():
+    el, coords = read_xyz("cut_fcc.xyz")
+    assert coordination_number(1, coords=coords , cut_off = 1.83)[25] == 9
+
+def test_gcn_111():
+    el, coords = read_xyz("cut_fcc.xyz")
+    assert agcn_calculator(1, coords=coords , cut_off = 1.83, gcn_max=12.0)[25] == 7.5
