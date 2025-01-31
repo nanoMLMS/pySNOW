5bb29797ab4b44502b024e92beaaa3056ad79101
nanoMLMS <nanoMLMS@fisica.unimi.it>
2025-01-31 16:13:17 +0100

    LAE addition for surface atoms, inner atoms, whole structure
    these functions calculate the local atomic environment of NP.
    To update:
    - delete previous file before creating a new one (now we are appending without any check. You should remove old file before running)
    - separate the calculation from the writing function

--- snow/lodispp/utils.py
+++ snow/lodispp/utils.py
@@ -331,4 +331,239 @@ def geometric_com(index_frame: int, coords: np.ndarray):
     
             
 
+def LAE_xyz(index_frame: int, coords: np.ndarray, elements, cut_off):
+
+    nearest_neigh = nearest_neighbours(index_frame, coords, cut_off)
     
+    elements_nn=[]
+    
+    for i in nearest_neigh:
+    
+        element_i_nn=[]
+        for el in i:
+            element_i_nn.append(elements[el])
+            
+        elements_nn.append(element_i_nn)
+    
+
+    print(elements_nn)
+        
+    num_atom_same_species=[]
+    num_atom_other_species=[]
+    
+    for j, elem in enumerate(elements_nn):
+        same=0
+        other=0
+        
+        for i in elem:
+            if i==elements[j]:
+                same=same+1
+            else:
+                other=other+1
+                
+        num_atom_same_species.append(same)
+        num_atom_other_species.append(other)
+        
+    
+    return num_atom_same_species, num_atom_other_species #to add in the xyz file
+    
+    
+def LAE_count(index_frame: int, coords: np.ndarray, elements, cut_off, elem_1, elem_2, range_lower_bin, range_upper_bin, savepath):
+    nearest_neigh = nearest_neighbours(index_frame, coords, cut_off)
+    
+    elements_nn = [[elements[el] for el in i] for i in nearest_neigh]
+    
+    num_atom_same_species = []
+    num_atom_other_species = []
+    num_NN = []
+    
+    for j, elem in enumerate(elements_nn):
+        same = sum(1 for i in elem if i == elements[j])
+        other = len(elem) - same
+        num_atom_same_species.append(same)
+        num_atom_other_species.append(other)
+        num_NN.append(len(elem))
+    
+    results = {elem_1: [0] * len(range_lower_bin), elem_2: [0] * len(range_lower_bin)}
+    
+    for r in range(len(range_lower_bin)):
+        for k, e in enumerate(elements):
+            if range_lower_bin[r] <= num_atom_other_species[k] and num_atom_other_species[k]<= range_upper_bin[r]:
+                results[e][r] += 1
+    
+    filename = f'{savepath}/LAE_count_results_{elem_1}.csv'
+    with open(filename, 'w') as f:
+    
+            
+        if index_frame==0:
+            f.write('Element, frame')
+            for r in range(len(range_lower_bin)):
+                f.write(f', [{range_lower_bin[r]}, {range_upper_bin[r]}]')
+            f.write('\n')
+            
+        
+        for elem in [elem_1, elem_2]:
+            f.write(f'{elem} , {index_frame}')
+            for count in results[elem]:
+                f.write(f', {count}')
+            f.write('\n')
+    
+    
+    filename = f'{savepath}/LAE_count_results_{elem_2}.csv'
+    with open(filename, 'w') as f:
+    
+            
+        if index_frame==0:
+            f.write('Element, frame')
+            for r in range(len(range_lower_bin)):
+                f.write(f', [{range_lower_bin[r]}, {range_upper_bin[r]}]')
+            f.write('\n')
+            
+        
+        for elem in [elem_2]:
+            f.write(f'{elem} , {index_frame}')
+            for count in results[elem]:
+                f.write(f', {count}')
+            f.write('\n')
+            
+            
+    print(f'Results saved in {filename}')
+
+        
+
+
+def LAE_surf_count(index_frame: int, coords: np.ndarray, elements, cut_off, elem_1, elem_2, range_lower_bin, range_upper_bin, savepath):
+    nearest_neigh = nearest_neighbours(index_frame, coords, cut_off)
+    agcn_array = agcn_calculator(index_frame, coords, cut_off, gcn_max = 12.0)
+    
+    print(agcn_array, len(agcn_array))
+    elements_nn = [[elements[el] for el in i] for i in nearest_neigh]
+    
+    num_atom_same_species = []
+    num_atom_other_species = []
+    num_NN = []
+    
+    for j, elem in enumerate(elements_nn):
+        same = sum(1 for i in elem if i == elements[j])
+        other = len(elem) - same
+        num_atom_same_species.append(same)
+        num_atom_other_species.append(other)
+        num_NN.append(len(elem))
+    
+    results = {elem_1: [0] * len(range_lower_bin), elem_2: [0] * len(range_lower_bin)}
+    
+
+    for r in range(len(range_lower_bin)):
+        n_surf=0
+        for k, e in enumerate(elements):
+            if agcn_array[k] <= 8.5:  # Consider only surface atoms
+                n_surf=n_surf+1
+                
+                #print('array: ', agcn_array[k] )
+                if range_lower_bin[r] <= num_atom_other_species[k] <= range_upper_bin[r]:
+                    results[e][r] += 1
+                    
+        print('n_surf ', n_surf)
+            
+    filename = f'{savepath}/LAE_count_results_surface_{elem_1}.csv'
+    with open(filename, 'a') as f:
+        if index_frame==0:
+                
+            f.write('Element, frame')
+            for r in range(len(range_lower_bin)):
+                f.write(f', [{range_lower_bin[r]}, {range_upper_bin[r]}]')
+            f.write('\n')
+        
+        for elem in [elem_1]:
+            f.write(f'{elem}, {index_frame}')
+            for count in results[elem]:
+                f.write(f', {count}')
+            f.write('\n')
+    
+    
+    
+    filename = f'{savepath}/LAE_count_results_surface_{elem_2}.csv'
+    with open(filename, 'a') as f:
+        if index_frame==0:
+            f.write('Element, frame')
+            for r in range(len(range_lower_bin)):
+                f.write(f', [{range_lower_bin[r]}, {range_upper_bin[r]}]')
+            f.write('\n')
+        
+        for elem in [elem_2]:
+            f.write(f'{elem}, {index_frame+1}')
+            for count in results[elem]:
+                f.write(f', {count}')
+            f.write('\n')
+            
+            
+    print(f'Surface results saved in {filename}')
+
+
+
+def LAE_inner_count(index_frame: int, coords: np.ndarray, elements, cut_off, elem_1, elem_2, range_lower_bin, range_upper_bin, savepath):
+    nearest_neigh = nearest_neighbours(index_frame, coords, cut_off)
+    agcn_array = agcn_calculator(index_frame, coords, cut_off, gcn_max = 12.0)
+    
+    print(agcn_array, len(agcn_array))
+    elements_nn = [[elements[el] for el in i] for i in nearest_neigh]
+    
+    num_atom_same_species = []
+    num_atom_other_species = []
+    num_NN = []
+    
+    for j, elem in enumerate(elements_nn):
+        same = sum(1 for i in elem if i == elements[j])
+        other = len(elem) - same
+        num_atom_same_species.append(same)
+        num_atom_other_species.append(other)
+        num_NN.append(len(elem))
+    
+    results = {elem_1: [0] * len(range_lower_bin), elem_2: [0] * len(range_lower_bin)}
+    
+
+    for r in range(len(range_lower_bin)):
+        n_inner=0
+        for k, e in enumerate(elements):
+            if agcn_array[k] > 8.5:  # Consider only surface atoms
+                n_inner=n_inner+1
+                
+                #print('array: ', agcn_array[k] )
+                if range_lower_bin[r] <= num_atom_other_species[k] <= range_upper_bin[r]:
+                    results[e][r] += 1
+                    
+        print('n_inner ', n_inner)
+            
+    filename = f'{savepath}/LAE_count_results_inner_{elem_1}.csv'
+    with open(filename, 'a') as f:
+        if index_frame==0:
+                
+            f.write('Element, frame')
+            for r in range(len(range_lower_bin)):
+                f.write(f', [{range_lower_bin[r]}, {range_upper_bin[r]}]')
+            f.write('\n')
+        
+        for elem in [elem_1]:
+            f.write(f'{elem}, {index_frame}')
+            for count in results[elem]:
+                f.write(f', {count}')
+            f.write('\n')
+    
+    
+    
+    filename = f'{savepath}/LAE_count_results_inner_{elem_2}.csv'
+    with open(filename, 'a') as f:
+        if index_frame==0:
+            f.write('Element, frame')
+            for r in range(len(range_lower_bin)):
+                f.write(f', [{range_lower_bin[r]}, {range_upper_bin[r]}]')
+            f.write('\n')
+        
+        for elem in [elem_2]:
+            f.write(f'{elem}, {index_frame+1}')
+            for count in results[elem]:
+                f.write(f', {count}')
+            f.write('\n')
+            
+            
+    print(f'Inner results saved in {filename}')
