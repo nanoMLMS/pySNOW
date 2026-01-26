f77701a39a49a367350e00dd9e7fc54c979f1f59
Giacomo Becatti <giacomo.becatti@unimi.it>
2026-01-26 09:33:20 +0100

    Added also lammps io to facilitation

--- snow/io/__init__.py
+++ snow/io/__init__.py
@@ -1,3 +1,3 @@
 from .xyz import read_xyz, read_xyz_movie, write_xyz
-
-__all__ = ["read_xyz", "read_xyz_movie", "write_xyz"]
+from .lammps import read_lammps_data,read_order_lammps_dump
+__all__ = ["read_xyz", "read_xyz_movie", "write_xyz", "read_order_lammps_dump", "read_lammps_data"]
