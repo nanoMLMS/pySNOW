d51bc42202d61c144d2f876fb5d495ccdfa2d0af
Giacomo Becatti <giacomo.becatti@unimi.it>
2026-02-02 11:46:05 +0100

    Added test cn

--- test/test_cn.py
+++ test/test_cn.py
@@ -17,6 +17,15 @@ def test_cn_twoatoms():
     assert coordination_number(1, coords=coords, cut_off = 3)[0] == 1
     assert coordination_number(1, coords=coords, cut_off = 1.5)[1] == 0
 
+def test_cn_threeatoms():
+    coords = np.array([
+        [0, 1, 0],
+        [0, 0, 0],
+        [0, -1, 0]
+    ])
+    els = np.array(["Au", "Au", "Au"])
+    assert coordination_number(1, coords=coords, cut_off = 3)[1] == 2
+
 import os
 
 def test_fcc_bulk():
