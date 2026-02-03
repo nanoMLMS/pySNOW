1a3026e96c11dbf50fedad464b0f64cc26ed169f
gilbogilb <nardi.gilbert@gmail.com>
2026-02-03 12:03:00 +0100

    added rotations and com shifting

--- snow/transform/rototranslation.py
+++ snow/transform/rototranslation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from snow.descriptors.shape_descriptors import center_of_mass as com, geometric_com as gcom
 
 
 def align_to_axis(index_frame: int, coords: np.ndarray, symmetry_axis: np.ndarray) -> np.ndarray:
@@ -47,3 +48,66 @@ def align_to_axis(index_frame: int, coords: np.ndarray, symmetry_axis: np.ndarra
 
     return translated_coords
     
+def center_com(index_frame : int, coords : np.ndarray, elements=None) -> np.ndarray:
+    """
+    Shifts the positions to the center of mass reference system (so that the center of mass is in the origin). 
+    If elements are provided, a mass-weighted average of positions is performed, otherwise (elements=None), a simple
+    geometrical average is used.
+
+    Parameters:
+        index_frame (int): Index of the frame in the trajectory - for reference only.
+        coords (np.ndarray): Array of atomic coordinates of shape (frames, atoms, 3).
+        elements (list or np.ndarray): List of element symbols corresponding to the atoms. Default to None.
+          If None, all positions will have the same weight in the calculation of the center of mass
+    
+    Returns:
+        np.ndarray: shifted coords
+    """
+
+    coords = np.asarray(coords)
+
+    if elements is not None:
+        return coords - com(index_frame, coords, elements)
+    else:
+        return coords - gcom(index_frame, coords)
+
+def rotate(coords, axis, angle):
+    """
+    Rotate coordinates around a given axis by a given angle.
+
+    Parameters
+    ----------
+    coords : array-like, shape (..., 3)
+        Coordinates to rotate.
+    axis : array-like, shape (3,)
+        Rotation axis (will be normalized).
+    angle : float
+        Rotation angle in radians.
+
+    Returns
+    -------
+    np.ndarray
+        Rotated coordinates, same shape as input.
+    """
+    coords = np.asarray(coords, dtype=float)
+
+    axis = np.asarray(axis, dtype=float)
+    n = np.linalg.norm(axis)
+    if n == 0:
+        raise ValueError("Rotation axis must be non-zero.")
+    axis = axis / n
+
+    x, y, z = axis
+    c = np.cos(angle)
+    s = np.sin(angle)
+    C = 1.0 - c
+
+    # Rodrigues' rotation matrix
+    R = np.array([
+        [c + x*x*C,     x*y*C - z*s, x*z*C + y*s],
+        [y*x*C + z*s,   c + y*y*C,   y*z*C - x*s],
+        [z*x*C - y*s,   z*y*C + x*s, c + z*z*C]
+    ])
+
+    # Apply rotation (works for shape (N,3) or (...,3))
+    return coords @ R.T
